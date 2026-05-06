e7acfcbdb134982d960a47fc65d3297b6e057569
tmccorquodale <thomas.mccorquodale@health.nsw.gov.au>
2026-05-06 12:04:44 +1000

    Set publicDir to 'Public' in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'Public',
   // Ensure assets are loaded relative to the root for GitHub Pages
   base: './',
   build: {
